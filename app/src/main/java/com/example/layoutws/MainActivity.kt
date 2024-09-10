c89a192edb87917ed68323128bb2c4ad3c1b9efe
pritam.kumar <pritam.kumar@samsung.com>
2024-09-10 22:35:49 +0530

    Added button for custom style

--- app/src/main/java/com/example/layoutws/MainActivity.kt
+++ app/src/main/java/com/example/layoutws/MainActivity.kt
@@ -9,20 +9,21 @@ import android.view.View
 import android.widget.GridView
 import android.widget.LinearLayout
 import com.example.layoutws.databinding.ActivityMainBinding
+import com.example.layoutws.databinding.ButtonLayoutBinding
 import com.example.layoutws.databinding.SingleProductItemBinding
 import com.example.layoutws.databinding.SubitemLayoutBinding
 import com.gtappdevelopers.kotlingfgproject.MyAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ButtonLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater) // inflate the layout
+        binding = ButtonLayoutBinding.inflate(layoutInflater) // inflate the layout
         setContentView(binding.root)
 
-        val mAdapater = MyAdapter(this, listOf(1,2,3,4,5,6,7,8,9,10,11,12,334,45,54,45,45,4235,6,57,6756))
-        binding.gridVW.adapter = mAdapater
+//        val mAdapater = MyAdapter(this, listOf(1,2,3,4,5,6,7,8,9,10,11,12,334,45,54,45,45,4235,6,57,6756))
+//        binding.gridVW.adapter = mAdapater
 
     }
 }
