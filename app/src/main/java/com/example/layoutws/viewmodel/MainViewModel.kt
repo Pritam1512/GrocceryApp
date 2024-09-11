2cdf365a3961e11bf426e324580cb82f5fdd0d0e
pritam.kumar <pritam.kumar@samsung.com>
2024-09-11 15:53:54 +0530

    Added livedata for example

--- app/src/main/java/com/example/layoutws/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/layoutws/viewmodel/MainViewModel.kt
@@ -1,14 +1,25 @@
 package com.example.layoutws.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     private var number = 0
+    private val myLiveData = MutableLiveData(1)
 
     fun addOne() {
         number++
     }
     fun getCount(): Int {
         return number
+    }
+
+    fun getMyLiveData(): MutableLiveData<Int> {
+        return myLiveData
+    }
+    fun setData(data: Int) {
+        Log.i("MainViewModel", Thread.currentThread().name)
+        myLiveData.postValue(data)
     }
 }
